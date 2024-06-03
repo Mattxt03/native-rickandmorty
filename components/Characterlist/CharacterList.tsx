a3b9f19526abb84146a0311c09745dcda6ce7b29
Mattxt03 <matteopgschool7@gmail.com>
2024-06-04 01:25:50 +0200

    refactor: Character list & types

--- components/Characterlist/CharacterList.tsx
+++ components/Characterlist/CharacterList.tsx
@@ -1,20 +1,50 @@
 import { View, Image, Text } from "react-native"
-import { CharacterCard } from "@/models/Character";
+import { CharacterCard } from "@/models/CharacterTypes";
+import { Link } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import useHandleFavs from "@/handlers/useHandleFavs";
+import AntDesign from "@expo/vector-icons/build/AntDesign";
 
-const CharacterList: React.FC<CharacterCard> = ({image, name, species, location, status}) => {
+const CharacterList: React.FC<CharacterCard> = ({id, image, name, species, status, location, gender}) => {
+
+    const { handleAddCharToFav } = useHandleFavs();
+
+
     return (
-        <View className="bg-white rounded-lg overflow-hidden shadow-md my-4 mx-4">
+    <View className="flex-row items-center rounded-lg overflow-hidden bg-white shadow-md my-4 mx-4">
       <Image
         source={{ uri: image }}
-        className="w-full h-48"
+        className="h-full w-1/2"
         resizeMode="cover"
       />
-      <View className="p-4">
-        <Text className="text-xl font-bold text-center mb-2">{name}</Text>
-        <Text className="text-gray-700 text-base text-center">{status}</Text>
-        <Text className="text-gray-700 text-base text-center">{species}</Text>
-        <Text className="text-gray-700 text-base text-center">{location}</Text>
+      <View className="flex-1 p-8 pl-4 ">
+        <Text className="text-xl font-bold  flex-wrap">{name}</Text>
+        <View className="flex-row items-center">
+          <View className={`w-2 h-2 ${status === 'Alive' ? 'bg-green-500' : status === 'Dead' ? 'bg-red-500' : 'bg-orange-500'} rounded-full`} />
+          <Text className="text-base pl-1">{status}</Text>  
+        </View> 
+        <Text className="">{species}</Text>
+        <TouchableOpacity className="mt-2 w-16 pl-1 border rounded-xl"> 
+        <Link 
+        href={{
+        pathname: '/details/[id]',
+        params: {id: id}
+        }}
+        >
+        Details {">"}
+      </Link>
+      </TouchableOpacity>
+      <TouchableOpacity className="mt-2 flex-row-reverse">
+      <AntDesign 
+      name="heart" 
+      size={24} 
+      color="black"
+      onPress={() => handleAddCharToFav({ id, species, name, status, image, location, gender })} />
+      </TouchableOpacity>
       </View>
+
+      
+      
     </View>
     )
 }
